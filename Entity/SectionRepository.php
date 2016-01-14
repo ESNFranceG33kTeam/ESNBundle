23f944a3b579579db0e2e2ffbfb83a8730901a06
Jeremie Samson <jeremie.samson76@gmail.com>
2016-01-14 18:15:57 +0100

    Add Section entity and Command to import them

---
+++ Entity/SectionRepository.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jerem
+ * Date: 14/01/16
+ * Time: 18:09
+ */
+
+namespace Esn\EsnBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * SectionRepository
+ */
+class SectionRepository extends EntityRepository
+{
+
+}
